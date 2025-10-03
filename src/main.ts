27d08afd61e4cd4a213326ffc72aab857eb1ee6a
mydoot <buaqur@gmail.com>
2025-10-03 02:21:25 +0000

    Created button click event

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@
 console.log("🎮 CMPM 121 - Starting...");
 
 // Simple counter for demonstration
-// deno-lint-ignore prefer-const
 let counter: number = 0;
 
 // Create basic HTML structure
@@ -21,6 +20,7 @@ const button = document.getElementById("increment")!;
 const counterElement = document.getElementById("counter")!;
 
 button.addEventListener("click", () => {
-  // This looks like to a good place to add some logic!
+  counter += 10;
+  counterElement.innerHTML = "" + counter + "";
   console.log("I have these thingies:", button, counterElement, counter);
 });
